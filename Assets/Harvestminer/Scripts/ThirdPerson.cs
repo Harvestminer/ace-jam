91636814690ae61f9aaa87d0ad9ea718a3b6955d
Harvestminer <harvestminer@gmail.com>
2024-03-02 02:26:40 -0600

    Adds world and player

---
+++ Assets/Harvestminer/Scripts/ThirdPerson.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPerson : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
